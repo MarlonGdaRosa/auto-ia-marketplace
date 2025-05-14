94eaea32f8764f6702db170bcee91414a9a38820
MarlonGdaRosa <canalapi.dev@gmail.com>
2025-05-14 15:57:38 +0000

    Remover botão whatsapp

--- src/pages/VehicleDetails.tsx
+++ src/pages/VehicleDetails.tsx
@@ -272,7 +272,7 @@ const VehicleDetails: React.FC = () => {
               <div className="space-y-3">
                 <ProposalForm vehicle={vehicle} buttonVariant="default" buttonFullWidth />
                 
-                {/* WhatsApp button 
+                {/* WhatsApp button
                 <Button variant="outline" className="w-full bg-green-50 border-green-300 text-green-700 hover:bg-green-100">
                   <svg
                     xmlns="http://www.w3.org/2000/svg"
@@ -289,7 +289,7 @@ const VehicleDetails: React.FC = () => {
                     <path d="M22 16.92v3a2 2 0 0 1-2.18 2 19.79 19.79 0 0 1-8.63-3.07 19.5 19.5 0 0 1-6-6 19.79 19.79 0 0 1-3.07-8.67A2 2 0 0 1 4.11 2h3a2 2 0 0 1 2 1.72 12.84 12.84 0 0 0 .7 2.81 2 2 0 0 1-.45 2.11L8.09 9.91a16 16 0 0 0 6 6l1.27-1.27a2 2 0 0 1 2.11-.45 12.84 12.84 0 0 0 2.81.7A2 2 0 0 1 22 16.92z"></path>
                   </svg>
                   Conversar via WhatsApp
-                </Button> */}
+                </Button>*/}
               </div>
             </div>
 
